7086d9139cec66df6e7f675a405a0c7b4db56860
Javi <javigm_87@hotmail.com>
2023-11-24 17:17:00 +0000

    Actualizamos el fichero de guardar.php

--- guardar.php
+++ guardar.php
@@ -1,11 +1,17 @@
 <?php
 include 'conexion.php';
 
-// Obtener la frase del formulario
+// Obtener los datos del formulario
 $frase = $_POST['frase'];
+$aux = $_POST['aux'];
+$campo = $_POST['campo'];
+$fechaPlanificada = $_POST['fechaPlanificada'];
+$fechaLimite = $_POST['fechaLimite'];
+$periodicidad = $_POST['periodicidad'];
 
 // Preparar la sentencia SQL
-$sql = "INSERT INTO frases (frase) VALUES ('$frase')";
+$sql = "INSERT INTO frases (frase, aux, campo, fecha_planificada, fecha_limite, periodicidad) 
+        VALUES ('$frase', '$aux', '$campo', '$fechaPlanificada', '$fechaLimite', '$periodicidad')";
 
 // Ejecutar la sentencia
 if ($conexion->query($sql) === TRUE) {
