467eab42d209352b357fa96945782b4680ae7472
Javi <javigm_87@hotmail.com>
2023-11-25 00:00:25 +0000

    Intento Arreglo 01

--- guardar.php
+++ guardar.php
@@ -9,6 +9,9 @@ $fechaPlanificada = $_POST['fechaPlanificada'];
 $fechaLimite = $_POST['fechaLimite'];
 $periodicidad = $_POST['periodicidad'];
 
+
+echo $frase;
+
 // Preparar la sentencia SQL
 $sql = "INSERT INTO frases (frase, aux, campo, fecha_planificada, fecha_limite, periodicidad) 
         VALUES ('$frase', '$aux', '$campo', '$fechaPlanificada', '$fechaLimite', '$periodicidad')";
