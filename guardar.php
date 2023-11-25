5735d0b1083043e457ef7e20178a10eb89268367
Javi <javigm_87@hotmail.com>
2023-11-25 00:13:35 +0000

    Intento02

--- guardar.php
+++ guardar.php
@@ -1,6 +1,9 @@
 <?php
 include 'conexion.php';
 
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 // Obtener los datos del formulario
 $frase = $_POST['frase'];
 $aux = $_POST['aux'];
@@ -10,7 +13,9 @@ $fechaLimite = $_POST['fechaLimite'];
 $periodicidad = $_POST['periodicidad'];
 
 
+print_r($_POST);
 echo $frase;
+
 
 // Preparar la sentencia SQL
 $sql = "INSERT INTO frases (frase, aux, campo, fecha_planificada, fecha_limite, periodicidad) 
